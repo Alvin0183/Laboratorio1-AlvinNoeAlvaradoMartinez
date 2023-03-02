7383eae60cc6a2300f5df78e201975cc11110b7a
Alvin0183 <alvinmartinez666@outlook.com>
2023-03-01 22:22:55 -0600

    Proyecto

---
+++ LABORATORIO1-AlvinNoeAlvaradoMartinez/LABORATORIO1-AlvinNoeAlvaradoMartinez/Negocio/ClsPersona.cs
@@ -0,0 +1,46 @@
+﻿using LABORATORIO1_AlvinNoeAlvaradoMartinez.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LABORATORIO1_AlvinNoeAlvaradoMartinez.Negocio
+{
+    public class ClsPersona
+    {
+
+        public string CalcularIMC(Persona pes)
+        {
+
+            double pespo = pes.Peso / Math.Pow(pes.Altura, 2);
+
+            if (pespo < 25)
+            {
+                return "Peso Ideal ";
+
+            }
+            else
+            {
+                return "Tiene sobrepeso ";
+            }
+
+
+        }
+
+        public string EsMayorDeEdad(Persona ed)
+        {
+            if (ed.Edad >= 18)
+            {
+                return "Es mayor de edad ";
+            }
+            else
+            {
+                return "Es menor de edad";
+            }
+
+        }
+    }
+}
+
+
---
+++ LABORATORIO1-AlvinNoeAlvaradoMartinez/LABORATORIO1-AlvinNoeAlvaradoMartinez/Program.cs
@@ -0,0 +1,30 @@
+﻿
+using LABORATORIO1_AlvinNoeAlvaradoMartinez.Entidades;
+using LABORATORIO1_AlvinNoeAlvaradoMartinez.Negocio;
+using System;
+
+Persona persona = new Persona();
+ClsPersona clspersona = new ClsPersona();
+
+
+
+Console.WriteLine("Ingresa tu Nombre: ");
+persona.Nombre = Console.ReadLine();
+
+Console.WriteLine("Ingresa tu edad: ");
+persona.Edad = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Dime tu sexo(H o M): ");
+persona.Sexo = Console.ReadLine();
+
+Console.WriteLine("Ingresa tu peso(Kg): ");
+persona.Peso = float.Parse (Console.ReadLine());
+
+Console.WriteLine("Ingresa tu Altura: ");
+persona.Altura = float.Parse (Console.ReadLine());
+
+
+
+
+Console.WriteLine(clspersona.CalcularIMC(persona));
+Console.WriteLine(clspersona.EsMayorDeEdad(persona));
